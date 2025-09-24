2a39580c32ad44ef444bf5c74d03c4680e77190e
Pontus <Pontus_thoren@hotmail.com>
2025-09-24 14:58:59 +0200

    changes from me

--- GitHubGruppOvning/Program.cs
+++ GitHubGruppOvning/Program.cs
@@ -1,30 +1,30 @@
 ﻿namespace GitHubGruppOvning
 {
-   internal class Program
-   {
-      static void Main(string[] args)
-      {
-         int numberOne;
-         int numberTwo;
-         int sum;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne;
+            int numberTwo;
+            int sum;
 
-         Console.WriteLine("Ange ditt val");
-         Console.WriteLine("1. Addera");
-         Console.WriteLine("2. Subtrahera");
-         Console.WriteLine("3. Multiplicera");
-         Console.WriteLine("4. Dividera");
-         int input;
-         while (!int.TryParse(Console.ReadLine(), out input))
-         {
-            Console.WriteLine("Du måste ange ett heltal");
-         }
+            Console.WriteLine("Ange ditt val");
+            Console.WriteLine("1. Addera");
+            Console.WriteLine("2. Subtrahera");
+            Console.WriteLine("3. Multiplicera");
+            Console.WriteLine("4. Dividera");
+            int input;
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Du måste ange ett heltal");
+            }
 
-         switch (input)
-         {
-            case 1:
-               // Addition
-               break;
-            case 2:
+            switch (input)
+            {
+                case 1:
+                    // Addition
+                    break;
+                case 2:
                     Console.WriteLine("Välj ditt första tal:");
                     numberOne = int.Parse(Console.ReadLine());
                     Console.WriteLine("Välj ditt andra tal.");
@@ -32,35 +32,35 @@
                     sum = numberOne - numberTwo;
                     Console.WriteLine($"{numberOne} - {numberTwo} = {sum} ");
                     break;
-            case 3:
-               Console.WriteLine("Ange tal 1: ");
-               while (!int.TryParse(Console.ReadLine(), out numberOne))
-               {
-                  Console.WriteLine(" Du måste ange heltal");
-               }
+                case 3:
+                    Console.WriteLine("Ange tal 1: ");
+                    while (!int.TryParse(Console.ReadLine(), out numberOne))
+                    {
+                        Console.WriteLine(" Du måste ange heltal");
+                    }
 
-               Console.WriteLine("Ange tal 2: ");
-               while (!int.TryParse(Console.ReadLine(), out numberTwo))
-               {
-                  Console.WriteLine(" Du måste ange heltal");
-               }
+                    Console.WriteLine("Ange tal 2: ");
+                    while (!int.TryParse(Console.ReadLine(), out numberTwo))
+                    {
+                        Console.WriteLine(" Du måste ange heltal");
+                    }
 
-               sum = numberOne * numberTwo;
+                    sum = numberOne * numberTwo;
 
-               Console.WriteLine($"{numberOne} * {numberTwo} = {sum}");
-               //Multiplikation
-               break;
-            case 4:
-               Console.WriteLine("Ange ditt första tal.");
-               numberOne = int.Parse(Console.ReadLine());
-               Console.WriteLine("Ange ditt andra tal.");
-               numberTwo = int.Parse(Console.ReadLine());
-               sum = numberOne / numberTwo;
-               Console.WriteLine($"{numberOne} / {numberTwo} = {sum}");
+                    Console.WriteLine($"{numberOne} * {numberTwo} = {sum}");
+                    //Multiplikation
+                    break;
+                case 4:
+                    Console.WriteLine("Ange ditt första tal.");
+                    numberOne = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ange ditt andra tal.");
+                    numberTwo = int.Parse(Console.ReadLine());
+                    sum = numberOne / numberTwo;
+                    Console.WriteLine($"{numberOne} / {numberTwo} = {sum}");
 
 
-               break;
-         }
-      }
-   }
+                    break;
+            }
+        }
+    }
 }
