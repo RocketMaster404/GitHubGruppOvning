6332215860049615eeea3e64748c1f5ee04ff137
Albin Linjer <albin.linjer@hotmail.com>
2025-09-24 14:45:14 +0200

    updated

--- GitHubGruppOvning/Program.cs
+++ GitHubGruppOvning/Program.cs
@@ -9,6 +9,10 @@
             int sum;
 
             Console.WriteLine("Ange ditt val");
+            Console.WriteLine("1. Addera");
+            Console.WriteLine("2. Subtrahera");
+            Console.WriteLine("3. Multiplicera");
+            Console.WriteLine("4. Dividera");
             int input;
             while (!int.TryParse(Console.ReadLine(), out input))
             {
@@ -18,30 +22,55 @@
             switch (input)
             {
                 case 1:
-                    {        // Addition
-                        Console.WriteLine("Skriv första talet:");
-                        numberOne = int.Parse(Console.ReadLine());
+                    // Addition
+                    // Addition
+                    Console.WriteLine("Skriv första talet:");
+                    numberOne = int.Parse(Console.ReadLine());
 
-                        Console.WriteLine("Skriv andra talet:");
-                        numberTwo = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv andra talet:");
+                    numberTwo = int.Parse(Console.ReadLine());
 
-                        sum = numberOne + numberTwo;
-                        Console.WriteLine("Summan är: " + sum);
-                        break;
-                    }
+                    sum = numberOne + numberTwo;
+                    Console.WriteLine("Summan är: " + sum);
+                    break;
+
                 case 2:
-                    // Subtraktion
+                    Console.WriteLine("Välj ditt första tal:");
+                    numberOne = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Välj ditt andra tal.");
+                    numberTwo = int.Parse(Console.ReadLine());
+                    sum = numberOne - numberTwo;
+                    Console.WriteLine($"{numberOne} - {numberTwo} = {sum} ");
                     break;
                 case 3:
+                    Console.WriteLine("Ange tal 1: ");
+                    while (!int.TryParse(Console.ReadLine(), out numberOne))
+                    {
+                        Console.WriteLine(" Du måste ange heltal");
+                    }
+
+                    Console.WriteLine("Ange tal 2: ");
+                    while (!int.TryParse(Console.ReadLine(), out numberTwo))
+                    {
+                        Console.WriteLine(" Du måste ange heltal");
+                    }
+
+                    sum = numberOne * numberTwo;
+
+                    Console.WriteLine($"{numberOne} * {numberTwo} = {sum}");
                     //Multiplikation
                     break;
                 case 4:
-                    // Division
+                    Console.WriteLine("Ange ditt första tal.");
+                    numberOne = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ange ditt andra tal.");
+                    numberTwo = int.Parse(Console.ReadLine());
+                    sum = numberOne / numberTwo;
+                    Console.WriteLine($"{numberOne} / {numberTwo} = {sum}");
+
+
                     break;
-
             }
-
-
         }
     }
 }
