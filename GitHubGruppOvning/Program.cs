4baa6cb7be5a23f63a07e4956b0a73972fdc2608
Pontus <Pontus_thoren@hotmail.com>
2025-09-24 15:04:37 +0200

    Small changes

--- GitHubGruppOvning/Program.cs
+++ GitHubGruppOvning/Program.cs
@@ -2,14 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            int numberOne;
-            int numberTwo;
-            int sum;
 
-    internal class Program
-    {
         static void Main(string[] args)
         {
             int numberOne;
@@ -31,13 +24,10 @@
             switch (input)
             {
                 case 1:
-                    // Addition
                     Console.WriteLine("Skriv första talet:");
                     numberOne = int.Parse(Console.ReadLine());
-
                     Console.WriteLine("Skriv andra talet:");
                     numberTwo = int.Parse(Console.ReadLine());
-
                     sum = numberOne + numberTwo;
                     Console.WriteLine("Summan är: " + sum);
                     break;
@@ -55,17 +45,13 @@
                     {
                         Console.WriteLine(" Du måste ange heltal");
                     }
-
                     Console.WriteLine("Ange tal 2: ");
                     while (!int.TryParse(Console.ReadLine(), out numberTwo))
                     {
                         Console.WriteLine(" Du måste ange heltal");
                     }
-
                     sum = numberOne * numberTwo;
-
                     Console.WriteLine($"{numberOne} * {numberTwo} = {sum}");
-                    //Multiplikation
                     break;
                 case 4:
                     Console.WriteLine("Ange ditt första tal.");
@@ -74,8 +60,6 @@
                     numberTwo = int.Parse(Console.ReadLine());
                     sum = numberOne / numberTwo;
                     Console.WriteLine($"{numberOne} / {numberTwo} = {sum}");
-
-
                     break;
             }
         }
